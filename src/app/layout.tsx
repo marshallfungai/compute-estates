dc75408481d49932397178ee9004bbe4a15fe467
marshall.m <marshall.m@nethouse.net>
2024-10-07 11:25:22 +0300

    Google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from 'next/font/google';
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
+
+const GA_TRACKING_ID = process.env.GOOGLE_TAG_ID; 
+
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -20,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={spaceGrotesk.className}>
+        <GoogleTagManager gtmId='G-VERZYXMRKP' />
         {children}
       </body>
     </html>
